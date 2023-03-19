a41736ee55e142f299d7894dc75f66b8477fcec2
Ian Kuznicki <iankuz25@gmail.com>
2023-03-19 19:33:22 -0300

    Started player class

---
+++ src/GameObjects/Player.h
@@ -0,0 +1,23 @@
+#pragma once
+
+
+namespace kuznickiGameObjects
+{
+	class Player
+	{
+	private:
+		
+
+	public:
+		Player();
+		~Player();
+	};
+
+	Player::Player()
+	{
+	}
+
+	Player::~Player()
+	{
+	}
+}
