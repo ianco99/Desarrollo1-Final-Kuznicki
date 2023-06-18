2827bd5cd9620693bf809c76245805ece89541f4
ianco99 <71154684+ianco99@users.noreply.github.com>
2023-06-18 19:32:02 -0300

    updated gitignore

--- src/GameObjects/Enemy.h
+++ src/GameObjects/Enemy.h
@@ -17,4 +17,12 @@ private:
 public:
 	Enemy();
 	~Enemy();
+
+	void Reposition(Vector2 newPos);
+	Vector2 GetPosition();
+	void SetVelocity(Vector2 newVelocity);
+	Vector2 GetVelocity();
+
+	void SetAcceleration(Vector2 newAcceleration);
+	Vector2 GetAcceleration();
 };
